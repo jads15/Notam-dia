e20be761b9ee6c7de8131aeff35f514ae5887bc6
João Antônio <77999403+jads15@users.noreply.github.com>
2021-07-17 21:25:47 -0300

    Create notamédia.js
    programa que calculo a média e mostra que foi aprovado ou reprovado

---
+++ notamédia.js
@@ -0,0 +1,12 @@
+var nota1= 7;
+ var nota2 = 7;
+ var Média = nota1 + nota2;
+ var Médiafinal =Média/2;
+ console.log("A média é",Médiafinal)
+ if(Média >=6){
+    console.log("Foi aprovado")
+
+}
+else{
+    console.log("Não foi aprovado")
+}
